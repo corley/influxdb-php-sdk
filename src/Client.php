698422918f3808c0e6e4dbd63c1a299a4517bf58
sam80180 <sam80180@gmail.com>
2019-06-02 16:34:35 +0800

    se puede especificar la precisión cuando hacer /query y /write.

--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,8 @@
 
 namespace InfluxDB;
 
+use InfluxDB\Adapter\Http\Reader as HttpReader;
+use InfluxDB\Adapter\Http\Writer as HttpWriter;
 use InfluxDB\Adapter\WritableInterface as Writer;
 use InfluxDB\Adapter\QueryableInterface as Reader;
 
@@ -12,6 +14,16 @@ class Client
 {
     private $reader;
     private $writer;
+
+    // las constantes de precisión
+    const PRECISION_NANOSECONDS = "ns";
+    const PRECISION_MICROSECONDS_U = "µ";
+    const PRECISION_MICROSECONDS = "u";
+    const PRECISION_MILLISECONDS = "ms";
+    const PRECISION_SECONDS = "s";
+    const PRECISION_MINUTES = "m";
+    const PRECISION_HOURS = "h";
+    const PRECISION_RFC3339 = "rfc3339";
 
     public function __construct(Reader $reader, Writer $writer)
     {
@@ -45,4 +57,26 @@ class Client
     {
         return $this->getReader()->query($query);
     }
+
+    public static function validatePrecision($strPrecision) {
+        return (in_array($strPrecision, [self::PRECISION_HOURS, self::PRECISION_MINUTES, self::PRECISION_SECONDS, self::PRECISION_MILLISECONDS, self::PRECISION_MICROSECONDS, self::PRECISION_MICROSECONDS_U, self::PRECISION_NANOSECONDS, self::PRECISION_RFC3339], TRUE) ? TRUE : FALSE);
+    } // fin validatePrecision()
+
+    public static function toValidQueryPrecision($strPrecision, $type=HttpReader::ENDPOINT) {
+        switch ($type) {
+            case HttpReader::ENDPOINT:
+                switch ($strPrecision) {
+                    case self::PRECISION_RFC3339: return NULL;
+                    case self::PRECISION_MICROSECONDS_U: return self::PRECISION_MICROSECONDS;
+                } // fin switch
+                break;
+            case HttpWriter::ENDPOINT:
+                switch ($strPrecision) {
+                    case self::PRECISION_RFC3339: return self::PRECISION_NANOSECONDS;
+                    case self::PRECISION_MICROSECONDS_U: return self::PRECISION_MICROSECONDS;
+                } // fin switch
+                break;
+        } // fin switch
+        return $strPrecision;
+    } // fin toValidQueryPrecision()
 }
